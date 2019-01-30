1ec09577a25acb4c316dd098905eec22814ccfef
Samantha Hyatt <shhyatt@gmail.com>
2019-01-30 03:30:35 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 const main = document.getElementById('main'); 
 
 main.addEventListener('click', function(event) {
-  alert('I was clicked!');
+  alert("I was clicked!");
 });
 
 const input = document.querySelector("input");
