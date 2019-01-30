2756308687be94d31acef80500ce72cfbc26f467
Samantha Hyatt <shhyatt@gmail.com>
2019-01-30 03:36:45 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,4 +3,13 @@
 
 
 
-const input = document.querySelector('input')
+const input = document.querySelector('input'); 
+
+input.addEventListener('keydown', function(e) {
+  if (e.key === "g") {
+    return e.preventDefault();
+  } else {
+    console.log(e.key);
+  
+  }
+});
